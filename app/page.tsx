c70777cf38d2e68b348b3d9e71f350e4e65b8456
ZakariaElkhadir <zelkhadir5@gmail.com>
2026-03-28 11:41:10 +0100

    Add global styles and update homepage layout with navigation and hero section
    - Introduced new color variables in globals.css for theming.
    - Updated page.tsx to include a header with navigation links and a hero section.
    - Added new images for the logo and hero section.
    - Included framer-motion and gsap as dependencies in package.json and package-lock.json.

--- app/page.tsx
+++ app/page.tsx
@@ -2,64 +2,75 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+    <>
+      <header className="mx-31 my-6 flex bg-bg-white items-center justify-between">
+        <nav className="flex flex-1 items-center">
+          <div className="logo">
+            <Image src="/logo.png" alt="Logo" width={48} height={48} />
+          </div>
+          <ul className="flex flex-1 items-center justify-center gap-12 text-base">
+            <li>
+              <a
+                href="#how-it-works"
+                className="text-text-nav-active"
+                aria-current="page"
+              >
+                How it works
+              </a>
+            </li>
+            <li>
+              <a href="#features" className="text-text-nav-inactive">
+                Features
+              </a>
+            </li>
+            <li>
+              <a href="#pricing" className="text-text-nav-inactive">
+                Pricing
+              </a>
+            </li>
+            <li>
+              <a href="#faq" className="text-text-nav-inactive">
+                FAQ
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <button className="bg-button text-white px-4 py-4.5 rounded-[43px]">
+          Get the app
+        </button>
+      </header>
+      <main>
+        <section className="flex flex-col items-center text-center">
+          <Image
+            src="/hero-image.png"
+            alt="Hero Image"
+            width={693}
+            height={598}
+          />
+          <h2 className="text-(length:--font-size-h2) leading-(--line-height-h2) text-text-vibrant">
+            Your money,<br/> captured instantly
+          </h2>
+          <p className="text-(length:--font-size-paragraph) leading-(--line-height-paragraph) text-text-muted">
+            A capture-first expense tracker that turns the most painful moment
+            in personal finance — logging a purchase — into a 5-second habit.
           </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+          <div className="inline-flex items-center justify-center rounded-[100px] border border-button p-1">
+            <button className="rounded-[100px] bg-button px-8 py-3 text-button-text">
+              Download
+            </button>
+            <button className="rounded-[100px] border border-button bg-transparent px-8 py-3 text-button">
+              Features
+            </button>
+          </div>
+        </section>
+        <section></section>
+        <section></section>
+        <section></section>
+        <section></section>
+        <section></section>
+        <section></section>
       </main>
-    </div>
+      <footer></footer>
+    </>
   );
 }
